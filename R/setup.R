78244939bd432710410a4c25868299cc6ce73d1a
amyheather <a.heather2@exeter.ac.uk>
2023-04-06 15:41:02 +0100

    Filename as input, and import packages

--- R/setup.R
+++ R/setup.R
@@ -1,10 +1,16 @@
 #' Load setup
 #'
+#' @param input_filename A string (name of file with model inputs)
+#'
 #' @return Runs setup.R script from inst/scripts
+#'
+#' @import tidyverse
+#' @importFrom readxl read_excel
+#'
 #' @export
 #'
 #' @examples
-#' load_setup()
-load_setup <- function() {
+#'
+load_setup <- function(input_filename) {
   source(system.file(package = "ipacs", "scripts/setup.R"))
 }
