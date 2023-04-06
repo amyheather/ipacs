74d4347cd15a3dd4edddf8336f5db99e81a1f471
amyheather <a.heather2@exeter.ac.uk>
2023-04-06 16:29:54 +0100

    Function imports all sheets, and vignette provides example of using that and assigning to appropriate dataframe names

--- R/import_file.R
+++ R/import_file.R
@@ -1,14 +1,17 @@
-#' Import file (test function)
+#' Imports the sheets from the excel file with the model parameters
+#' File should contain sheets "arrivals", "initial conditions", "capacity",
+#' "los" and "costs"
 #'
 #' @param filename string
-#' @param sheet string
 #'
 #' @importFrom readxl read_excel
 #'
-#' @return file dataframe
+#' @return list(df_names, df_list)
 #'
 #' @export
-import_file <- function(filename, sheet){
-  file <- readxl::read_excel(filename, sheet=sheet)
-  return(file)
+import_file <- function(filename){
+  sheets <- c("arrivals", "initial conditions", "capacity", "los", "costs")
+  df_names <- c("arrivals_all", "init_conds", "capacity", "losA", "costs")
+  df_list <- lapply(sheets, function(x) readxl::read_excel(filename, sheet = x))
+  return(list(df_names, df_list))
 }
